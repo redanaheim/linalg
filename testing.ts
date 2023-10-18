064bb53d1d4187de2c2f0a787352c02db31ec64f
TigerGold59 <54746609+redanaheim@users.noreply.github.com>
2023-10-18 15:02:42 -0400

    accidentally added ignored file

--- testing.ts
+++
@@ -1,8 +0,0 @@
-import { Vector, Matrix, SwapRowOp, SetRowOp, AddRowOp } from "./index.js";
-
-let x = Vector.from_array([3, 7, 13]);
-
-let b = [Vector.from_array([1,1,1]), Vector.from_array([0,1,1]), Vector.from_array([0,0,1])]
-
-let x_coords = x.with_basis(3, ...b) ?? Vector.from_array([-1000000000]);
-console.log(x_coords.toString())
